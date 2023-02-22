12caac0f16c9e515c9f8c323001cda717e1baa36
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-22 17:58:07 -0300

    Criando states iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const startingGame = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  const [game, setGame] = useState(startingGame);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [playing, setPlaying] = useState(true);
+
   return (
     <>
-      <h1>Ready to code</h1>
+      <h1>Ready to code X O</h1>
     </>
   );
 }
