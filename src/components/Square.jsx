d4485bc2140dc19a3ec53d904f2898c958ea2eb7
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-23 17:40:54 -0300

    Corrigindo bugs visuais

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,57 @@
 import "./Square.css";
 
 const Square = ({ game, line, square, onClick }) => {
+  let styleSquare = {};
+
+  if (line === 0) {
+    if (square === 0)
+      styleSquare = {
+        borderRight: "2px solid #0099dd",
+        borderBottom: "2px solid #0099dd",
+      };
+
+    if (square === 1)
+      styleSquare = {
+        borderRight: "2px solid #0099dd",
+        borderBottom: "2px solid #0099dd",
+      };
+    if (square === 2) styleSquare = { borderBottom: "2px solid #0099dd" };
+  }
+
+  if (line === 1) {
+    if (square === 0)
+      styleSquare = {
+        borderRight: "2px solid #0099dd",
+        borderBottom: "2px solid #0099dd",
+      };
+
+    if (square === 1)
+      styleSquare = {
+        borderRight: "2px solid #0099dd",
+        borderBottom: "2px solid #0099dd",
+      };
+    if (square === 2) styleSquare = { borderBottom: "2px solid #0099dd" };
+  }
+
+  if (line === 2) {
+    if (square === 0)
+      styleSquare = {
+        borderRight: "2px solid #0099dd",
+      };
+
+    if (square === 1)
+      styleSquare = {
+        borderRight: "2px solid #0099dd",
+      };
+    if (square === 2) styleSquare = { border: "none" };
+  }
+
   return (
     <div
       className="square"
       data-pos={`${line}${square}`}
       onClick={(e) => onClick(e)}
+      style={styleSquare}
     >
       {game[line][square]}
     </div>
