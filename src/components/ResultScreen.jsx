a64355dac13035ad0fd0dda2b3ce49c4e3d479f8
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-24 20:45:19 -0300

    Criando animação para o resultado

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -7,8 +7,8 @@ const ResultScreen = ({ winner, onRestart }) => {
     <div className="result-screen">
       <div className="result-message">
         <p>{winner ? `Jogador ${winner} venceu!` : "Empate!"}</p>
+        <Button onClick={onRestart}>Jogar novamente</Button>
       </div>
-      <Button onClick={onRestart}>Jogar novamente</Button>
     </div>
   );
 };
