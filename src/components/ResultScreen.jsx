0e1356ecb2a346d6005125f85dc18028161f4127
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-24 21:02:34 -0300

    Adicionando efeito sonoro

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+
 import Button from "./Button";
+import victorySound from "../assets/victorysound.mp3";
 
 import "./ResultScreen.css";
 
 const ResultScreen = ({ winner, onRestart }) => {
+  useEffect(() => {
+    const audio = new Audio(victorySound);
+    audio.volume = 0.4;
+    audio.play();
+  }, []);
+
   return (
     <div className="result-screen">
       <div className="result-message">
